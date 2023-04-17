d687abc37f046b33d67e8ca0404d8af3dee6c394
Swapnamudra Das <swapnamudradas@Swapnamudras-MacBook-Air.local>
2023-04-17 23:53:12 +0800

    right pattern star

--- src/main/java/com/javacode/practice/RightPatternStar.java
+++ src/main/java/com/javacode/practice/RightPatternStar.java
@@ -1,8 +1,24 @@
 package com.javacode.practice;
 
+import java.util.Scanner;
+
 public class RightPatternStar {
     
     public static void main(String[] args) {
+
+        int i,j, row;
+        Scanner sc=new Scanner(System.in);
+        row=sc.nextInt();
+        System.out.print("Enter row number:\n");
+        for(i=0;i<=row;i++)
+        {
+            for(j=0;j<=i;j++)
+            {
+              System.out.print("*");
+            }
+            System.out.println("\n");
+
+        }
     
         
         
