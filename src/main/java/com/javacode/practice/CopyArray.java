2f178ba03712cc64764de0c026733fa0efe84cfd
Swapnamudra Das <swapnamudradas@Swapnamudras-MacBook-Air.local>
2023-04-18 23:53:45 +0800

    copy array code

---
+++ src/main/java/com/javacode/practice/CopyArray.java
@@ -0,0 +1,33 @@
+package com.javacode.practice;
+
+public class CopyArray {
+
+ //Program to copy all elements of one array into another array  
+    /* Ex:
+    ARRAY 1  
+    1   2  3  4  5  
+    ARRAY 2  
+    1   2  3  4  5   */
+
+public static void main(String[] args) {
+
+    int arr1[]=new int[]{1,2,3,4,5};
+    int arr2[]=new int[arr1.length];
+
+    for(int i=0;i<arr1.length;i++){
+       arr2[i]=arr1[i];
+    }
+    System.out.println("Enter original array:");
+    for(int j=0;j<arr1.length;j++){
+        System.out.print(arr1[j]+" ");
+    }
+    System.out.println();
+    System.out.println("Element of new array:");
+
+    for(int k=0;k<arr2.length;k++){
+        System.out.print(arr2[k]+" ");
+    }
+    
+}
+    
+}
