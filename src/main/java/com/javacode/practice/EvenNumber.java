821f3724685e4b60b997a302f89df5e44a652ec9
Swapnamudra Das <swapnamudradas@Swapnamudras-MacBook-Air.local>
2023-04-18 15:14:24 +0800

    Evennumber code

---
+++ src/main/java/com/javacode/practice/EvenNumber.java
@@ -0,0 +1,35 @@
+package com.javacode.practice;
+
+import java.util.Scanner;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+        
+        int i,count=100;
+        for(i=1;i<=count;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.print(i+" ");
+            }
+        }     
+
+
+        /* 
+    int number;
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter number:\n");
+    number=sc.nextInt();
+
+        if(number%2==0)
+            {
+            System.out.print(number+ ":is even number");
+            }
+        else
+            {
+                System.out.print(number+ ":is not even number");
+            }      */
+                
+    }  
+
+}
